891bf08cb86e3b56f98f27787f1b97f6d7dea3f4
Guryanova.Kristina <cristina.guryanova2017@yandex.ru>
2025-08-19 11:28:28 +0700

    feat: Логирование

--- main.cpp
+++ main.cpp
@@ -1,11 +1,49 @@
 #include "mainwindow.h"
 
+#include <QFile>
 #include <QApplication>
+
+#include <iostream>
+
+using namespace std;
+
+QScopedPointer<QFile> m_logFile;
+
+void messageHandler(QtMsgType type, const QMessageLogContext &context, const QString &msg);
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
+
+    m_logFile.reset(new QFile("../logFile.txt"));
+    m_logFile.data()->open(QFile::Append | QFile::Text);
+    qInstallMessageHandler(messageHandler);
+
     MainWindow w;
     w.show();
     return a.exec();
+}
+
+void messageHandler(QtMsgType type, const QMessageLogContext &context, const QString &msg)
+{
+    Q_UNUSED(context)
+
+    QString logMessage = QDateTime::currentDateTime().toString("yyyy-MM-dd hh:mm:ss.zzz ");
+
+    switch (type) {
+        case QtInfoMsg:     logMessage += "[INFO] "; break;
+        case QtDebugMsg:    logMessage += "[DEBUG] "; break;
+        case QtWarningMsg:  logMessage += "[WARNING] "; break;
+        case QtCriticalMsg: logMessage += "[CRITICAL] "; break;
+        case QtFatalMsg:    logMessage += "[FATAL] "; break;
+    }
+
+    logMessage += ": ";
+    logMessage += msg;
+
+    QTextStream out(m_logFile.data());
+    out << logMessage << Qt::endl;
+    out.flush();
+
+    cout << logMessage.toStdString() << endl;
 }
